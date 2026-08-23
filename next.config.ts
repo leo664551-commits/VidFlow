2edd3a9dda6a41aab4f42f56ae65bc69906271bc
subarnnnnnnnnnn75-create <subarnaghimire75@gmail.com>
2026-08-23 14:09:36 +0545

    fix(vercel): make output standalone conditional to resolve nft.json ENOENT error on Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  ...(process.env.STANDALONE === "true" ? { output: "standalone" } : {}),
   serverExternalPackages: ["bcryptjs"],
   allowedDevOrigins: ["*"],
   devIndicators: false,
